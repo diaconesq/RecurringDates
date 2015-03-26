ba745d12ec3ece040a982b11f962def49d2fb16a
cdiaconescu <cristian@diaconesq.ro>
2015-03-27 01:53:38 +0200

    NuGet package

--- RecurringDates/Properties/AssemblyInfo.cs
+++ RecurringDates/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("CalendarRules")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("RecurringDates")]
+[assembly: AssemblyDescription("Recurring dates based on programmatic recurrence rules. For instance: 'on Tuesdays and Fridays, except the 3rd week of the month, in June and August'")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("CalendarRules")]
+[assembly: AssemblyCompany("Cristian Diaconescu")]
+[assembly: AssemblyProduct("RecurringDates")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.5.0.0")]
+[assembly: AssemblyFileVersion("0.5.0.0")]
---
+++ RecurringDates/readme.txt
@@ -0,0 +1,3 @@
+RecurringDates
+
+Go to https://github.com/diaconesq/RecurringDates for any questions / bugs / improvement suggesions etc.
